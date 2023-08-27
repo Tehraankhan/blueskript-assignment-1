e6f0a03b5d5a3bc2d50c9cb093c61a83554a170b
Tehraan khan <20co49@aitdgoa.edu.in>
2023-08-27 11:52:11 +0530

    updated list.js file or component

--- src/Component/List.js
+++ src/Component/List.js
@@ -6,12 +6,12 @@ import React, { useEffect, useState } from 'react';
 
 
 function List(props) {
-    const [line, setline] = useState(false)
+
 
 
     const checked = (e) => {
         if (e.target.checked) {
-            setline(true)
+
 
             props.check(props.id, true)
 
@@ -19,7 +19,7 @@ function List(props) {
         else {
 
 
-            setline(false)
+
             props.check(props.id, false)
 
 
@@ -28,17 +28,9 @@ function List(props) {
         }
 
     }
-    useEffect(() => {
-
-        if (props.complete) {
-            setline(true)
-        }
-        else {
-            setline(false)
-        }
-    }, [])
 
 
+    console.log(props.complete)
 
 
     return (
@@ -47,8 +39,8 @@ function List(props) {
             <div className='flex flex-row bg-[#c4f9ff85] w-[346px] h-[4.5rem] mx-[auto] rounded-[5px] mt-[18px] sm:mx-[auto] sm:w-[450px] md:w-[600px] md:h-[5rem] lg:w-[700px] lg:mx-[auto] xl:w-[900px] 2xl:w-[900px] 2xl:mx-[auto]'>
 
 
-                <input className="basis-[19px] w-[1px] h-[13px] mt-8 ml-2" type="checkbox" onClick={checked} defaultChecked={props.complete} ></input>
-                <h1 className=" w-[800px] mt-5 text-[21px]" style={{ textDecoration: line ? 'line-through' : 'none' }}>{props.title}</h1>
+                <input className="basis-[19px] w-[1px] h-[13px] mt-8 ml-2" type="checkbox" onClick={checked} checked={props.complete} ></input>
+                <h1 className=" w-[800px] mt-5 text-[21px]" style={{ textDecoration: props.complete ? 'line-through' : 'none' }}>{props.title}</h1>
 
 
                 <button onClick={() => props.onDelete(props.id)}>
